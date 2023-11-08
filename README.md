6eba82c3df929d2b782c319d51bc272d2f978141
Sean <39905403+pricklygorse@users.noreply.github.com>
2023-11-08 20:42:06 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+# Inkscape Quick Measure Extension
+
+A super simple extension to simply output the length of a selected path or paths (straight or beziers), including the individual segment lengths and cumulative lengths.
+
+Has been tested most on Inkscape 1.2 and 1.3. *Should* work on 1.1, but I recommend updating. Inkscape has improved so much.
+
+
+## Installation
+
+Add the .inx and .py files to your Inkscape user extensions folder. You can find this by opening Inkscape Preferences (Edit-Preferences) and selecting the System tab. Restart Inkscape and the extension will appear under Extensions - Prickly Gorse - Quick Measure.
+
+I plan to release more related extensions soon, which will all be placed under the Prickly Gorse heading.
+
+## Usage
+
+Select your path (or multiple individual paths) and run the extension.
+
+Shapes need to be converted to paths (Object - Object to Path).
+
+# Prickly Gorse Sewing Guides
+
+I wrote this to assist with making sewing patterns for bike and backpacking bags. Bit of a shameless plug, but if you are interested in sewing your own outdoor gear without making your own patterns, check out [www.payhip.com/pricklygorse](http://www.payhip.com/pricklygorse) for my sewing guides (some free ones :D ), or [www.pricklygorsegear.com/tutorials-myog](http://www.pricklygorsegear.com/tutorials-myog) for my main website.
+
+You can also donate at this [link](https://www.paypal.com/donate/?business=WBEASYMGED4X8) if you find it useful :)
+
+# Acknowledgements
+
+The code is built from the quite a bit more complicated Measure Path extension that comes pre-installed with Inkscape. Huge thanks to the authors of those extensions, and everyone who has contributed to the Inkscape project as a whole. Support open source!
---
+++ quick_measure.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# coding=utf-8
+# inkscape 1.2
+
+import inkex
+
+from inkex.bezier import csplength
+
+
+class MeasureLength(inkex.EffectExtension):
+   
+    
+    def add_arguments(self, pars):
+       pars.add_argument(
+            "-u", "--unit", default="px", help="The unit of the measurement"
+        )
+
+    def effect(self):
+       
+        
+        try:
+                 if float(inkex.__version__[:3]) >= 1.2:
+                   viewport_code = True
+                   
+                 else:
+                   viewport_code = False
+                  
+        except Exception:
+                 
+                 viewport_code = False
+        
+        
+        
+        
+        # Calculate the unit factor depending on the inkscape version
+        # unittouu is not officially depreciated but is not recommended
+        # The output of unittouu has changed in the past.
+        
+        if viewport_code == False:
+        
+          #############################
+          # inkscape 1.1 code block
+          prec = 2
+          scale = self.svg.unittouu('1px')  # convert to document units
+          factor = 1.0
+
+          if self.svg.get('viewBox'):
+            factor = self.svg.scale / self.svg.unittouu('1px')
+
+          factor *= scale / self.svg.unittouu('1' + self.options.unit)
+          
+        
+        else:
+          
+          #inkscape 1.2 and newer
+          prec = 2
+          scale = self.svg.viewport_to_unit(
+            "1" + self.svg.document_unit
+          )  # convert to document units
+          factor = self.svg.unit_to_viewport(1, self.options.unit)
+        
+
+
+        # loop over all selected paths
+        filtered = self.svg.selection.filter(inkex.PathElement)
+        
+        
+        if not filtered:
+            raise inkex.AbortExtension(_("Please select at least one path object."))
+        
+        for node in filtered:
+            
+            csp = node.path.transform(node.composed_transform()).to_superpath()
+                        
+            slengths, stotal = csplength(csp)
+            
+            # convert segment lengths into user defined units.
+            slengths_uu = []
+            cumulative = [0]
+            count = 0
+            for number in slengths[0]:
+                 length_user_unit = round(number * factor, prec)
+  
+                 slengths_uu.append(length_user_unit)
+                 
+                 count += number
+                 
+                 cumulative.append(round(count * factor,prec))
+        
+            self.msg("Total length: " + str(round(stotal * factor,2)))
+            self.msg("Segment Lengths: " + str(slengths_uu))
+            self.msg("Cumulative Lengths: " + str(cumulative))
+            self.msg("")
+
+           
+if __name__ == "__main__":
+    MeasureLength().run()
